1dc10b707ff363e2c979e06963946bf1364db03c
Nikola Đuza <nikolaseap@gmail.com>
2021-12-06 15:41:25 +0100

    Change other forms to Form

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,5 +1,11 @@
 import { User } from "@prisma/client";
-import { Link, LinksFunction, LoaderFunction, useLoaderData } from "remix";
+import {
+  Link,
+  Form,
+  LinksFunction,
+  LoaderFunction,
+  useLoaderData,
+} from "remix";
 import { Outlet } from "remix";
 import { db } from "~/utils/db.server";
 import { getUser } from "~/utils/session.server";
@@ -50,11 +56,11 @@ export default function JokesRoute() {
           {data.user ? (
             <div className="user-info">
               <span>{`Hi ${data.user.username}`}</span>
-              <form action="/logout" method="post">
+              <Form action="/logout" method="post">
                 <button type="submit" className="button">
                   Logout
                 </button>
-              </form>
+              </Form>
             </div>
           ) : (
             <Link to="/login">Login</Link>
